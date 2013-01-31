5ab0e01e938466453a7f0ef1827be9edcf055d3e
Delons <delonsiv@gmail.com>
2013-01-31 15:39:12 -0500

    Added Blocks

--- src/Blocks.java
+++ src/Blocks.java
@@ -1,4 +1,14 @@
 
 public class Blocks {
 
+	String name;
+	
+	public Blocks(String name, int r, int g, int b){
+		this.name = name;
+	}
+	
+	Blocks air = new Blocks("air", 154, 213, 248);
+	Blocks dirt = new Blocks("dirt", 96, 73, 49);
+	Blocks stone = new Blocks("stone", 120, 120, 120);
+	
 }
