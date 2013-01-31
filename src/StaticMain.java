888a6bb14617ce4f57a3c2f17d23b395cdd010d2
Delons <delonsiv@gmail.com>
2013-01-31 15:11:52 -0500

    Started Coding

--- src/StaticMain.java
+++ src/StaticMain.java
@@ -1,4 +1,9 @@
 
 public class StaticMain {
 
+	public static void main(String args[]){
+		Main main = new Main();
+		main.startGame();
+	}
+	
 }
--- src/Terrain.java
+++ src/Terrain.java
@@ -1,4 +1,15 @@
 
 public class Terrain {
 
+	final int blockSize = 15;
+	int[][] land;
+	
+	public void generate(){
+		
+		land = new int[0][0];
+		
+		
+		
+	}
+	
 }
