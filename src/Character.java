3f96e572cfe1fc62a4c142bec0791d24cfb56fde
Delons <delonsiv@gmail.com>
2013-02-01 13:13:39 -0500

    Added Variables and Void's

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,18 @@
-
 public class Character {
+
+  int x, y, health;
+  
+  public Character(){
+    health = 100;
+    
+  }
+  
+  public void move(){
+    
+  }
+  
+  public void jump(){
+    
+  }
 
 }
